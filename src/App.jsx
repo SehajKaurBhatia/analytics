f5507576277e25323bb6913e458ab0c27a476f69
SehajKaurBhatia <sebhat1504@gmail.com>
2024-07-09 01:32:55 +0530

    add page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import AnalyticsPage from './pages/AnalyticsPage'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <AnalyticsPage/>
+    </>
+  )
+}
+
+export default App
